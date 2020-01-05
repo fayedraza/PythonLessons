cd0b4b5e66e1ceb0c11f9f89cb895b97e340e071
fayed1234@hotmail.com <fayed1234@hotmail.com>
2020-01-05 02:25:20 -0500

    Commit

---
+++ ex30.py
@@ -0,0 +1,13 @@
+people = 30
+cars =40
+trucks =15
+
+if cars > people:
+    print("We should take the cars.")
+elif cars < people:
+    print("We should not take the car")
+else:
+    print("We still can't decicde")
+    
+        
+
---
+++ ex31.py
@@ -0,0 +1,8 @@
+print("""You enter a dark room with two doors.
+Do you go through door 1 or 2? """)
+
+door = "12"
+
+if door == "1":
+    print("They are equal")
+    print("Prints out other line")
