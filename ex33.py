c4313c7cb015758853f8b7899a08a563d69500fa
fayed1234@hotmail.com <fayed1234@hotmail.com>
2020-01-06 02:25:37 -0500

    commit

---
+++ ex33.py
@@ -0,0 +1,16 @@
+i=0
+numbers =[]
+
+
+while i <6:
+    print(i)
+    numbers.append(i)
+
+    i=i+1
+    print("Numbers are now: ", numbers)
+    print(f"Bottom: {i}")
+
+print("The numbers: ")
+
+for a in  numbers:
+    print(a)
